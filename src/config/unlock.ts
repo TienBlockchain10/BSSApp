1dd80e7be74f3da05bbe0a30a708b82c47e893e7
unknown <nguyensecure@proton.me>
2023-04-12 22:25:09 -0500

    mabye try original

--- src/config/unlock.ts
+++ src/config/unlock.ts
@@ -1,17 +1,32 @@
+// export const paywallConfig: Record<string, unknown> = {
+//   "icon": "https://gateway.pinata.cloud/ipfs/QmZybHbp7uTguzkcFaZRtg2Pde1bWfiq37nz2bLPrdQTxa",
+//   "locks": {
+//     "0xf88f3765d08d674f464efa72a6866b1e965d3698": {
+//       "name": "",
+//       "promo": false,
+//       "network": 5,
+//       "dataBuilder": "",
+//       "emailRequired": true,
+//       "maxRecipients": null,
+//       "skipRecipient": true
+//     }
+//   },
+//   "title": "Claim Your Digital Collectible",
+//   "pessimistic": true,
+//   "skipRecipient": true
+// };
+
+
+
 export const paywallConfig: Record<string, unknown> = {
-  "icon": "https://gateway.pinata.cloud/ipfs/QmZybHbp7uTguzkcFaZRtg2Pde1bWfiq37nz2bLPrdQTxa",
-  "locks": {
-    "0xf88f3765d08d674f464efa72a6866b1e965d3698": {
-      "name": "",
-      "promo": false,
-      "network": 5,
-      "dataBuilder": "",
-      "emailRequired": true,
-      "maxRecipients": null,
-      "skipRecipient": true
-    }
+  locks: {
+    "0x77d11b698808eef99e32d3273dc88cc34097786f": {
+      network: 5,
+      name: "wonderwomancode",
+    },
   },
-  "title": "Claim Your Digital Collectible",
-  "pessimistic": true,
-  "skipRecipient": true
+  pessimistic: false,
+  title: "Super Hero Zone",
+  icon: "https://pin.ski/3Fc5Dqa",
+  persistentCheckout: false,
 };
