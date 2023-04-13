2293c6c3267050581b2924a09c4bfaac8894ce4a
unknown <nguyensecure@proton.me>
2023-04-12 21:53:58 -0500

    second commit

---
+++ src/config/unlock.ts
@@ -0,0 +1,17 @@
+export const paywallConfig: Record<string, unknown> = {
+  "icon": "https://gateway.pinata.cloud/ipfs/QmZybHbp7uTguzkcFaZRtg2Pde1bWfiq37nz2bLPrdQTxa",
+  "locks": {
+    "0xf88f3765d08d674f464efa72a6866b1e965d3698": {
+      "name": "",
+      "promo": false,
+      "network": 5,
+      "dataBuilder": "",
+      "emailRequired": true,
+      "maxRecipients": null,
+      "skipRecipient": true
+    }
+  },
+  "title": "Claim Your Digital Collectible",
+  "pessimistic": true,
+  "skipRecipient": true
+};
