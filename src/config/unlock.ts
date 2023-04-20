9c27a01f88fe4733694c4d1ab6615189c90a1f10
unknown <nguyensecure@proton.me>
2023-04-20 14:12:14 -0500

    fixed it to old v2 contract

--- src/config/unlock.ts
+++ src/config/unlock.ts
@@ -1,10 +1,42 @@
-export const paywallConfig: Record<string, unknown> = {
+export const paywallConfig: Record<string, unknown> =
 
+
+// {
+
+//   "icon": "https://i.ibb.co/phCKXKM/BLOCKCHAIN-STUDENT-SOCIETY-01.png",
+//   "locks": {
+//     "0xf88f3765d08d674f464efa72a6866b1e965d3698": {
+//       "name": "BSS General Membership",
+//       "promo": false,
+//       "network": 5,
+//       "dataBuilder": "",
+//       "emailRequired": true,
+//       "maxRecipients": 1,
+//       "skipRecipient": true
+//     }
+//   },
+//   "title": "Claim Your Digital Collectible",
+//   "hideSoldOut": false,
+//   "pessimistic": true,
+//   "messageToSign": "You ready bruv?",
+//   "skipRecipient": true,
+//   "endingCallToAction": "Enter BSS",
+//   "persistentCheckout": false
+// //old version with 16
+// };
+
+
+{
   "icon": "https://i.ibb.co/2ndZDNM/BLOCKCHAIN-STUDENT-SOCIETY-MORE.jpg",
   "locks": {
-    "0xa926ad36faf54e5738913ea99fe0a2a540cd4911": {
-      "name": "BSS General Membership",
-      "network": 5
+    "0x8af8525f9b00f3b5bae43512d6cfeef8f9555272": {
+      "name": "Testing Premium (monthly)",
+      "promo": false,
+      "network": 5,
+      "dataBuilder": "",
+      "emailRequired": true,
+      "maxRecipients": null,
+      "skipRecipient": false
     }
   },
   "title": "Claim Your Membership Card",
@@ -14,8 +46,4 @@ export const paywallConfig: Record<string, unknown> = {
   "skipRecipient": true,
   "endingCallToAction": "Enter BSS",
   "persistentCheckout": false
-
-};
-
-
-
+}
